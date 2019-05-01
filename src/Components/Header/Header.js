18e0a5eeb6fa483786b8760fc4e7d29107ac2f8f
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-04-30 23:26:42 -0600

    Add buttons

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,6 +5,11 @@ const Header = () => {
   return (
     <header>
       <div className="top-nav" />
+      <div className="button-container">
+        <input type="submit" className="people" value="PEOPLE" />
+        <input type="submit" className="planets" value="PLANETS" />
+        <input type="submit" className="vehicles" value="VEHICLES" />
+      </div>
     </header>
   );
 };
