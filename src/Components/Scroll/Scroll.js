1960d812d9cdbd256076edaf17d44c24ebd902c0
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-05-01 03:01:20 -0600

    Logo animation

--- src/Components/Scroll/Scroll.js
+++ src/Components/Scroll/Scroll.js
@@ -5,8 +5,12 @@ const Scroll = props => {
   return (
     <section className="scrollContainer">
       <p className="openingText">
+        {" "}
         A long time ago in a galaxy far, far away....
       </p>
+      <section className="logo">
+        <img src={require("../../assets/logo.svg")} alt="Star Wars Logo" />
+      </section>
     </section>
   );
 };
