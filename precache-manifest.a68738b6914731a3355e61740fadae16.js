53607a1ee1f76585d1e03e87fbb6c8bda206a580
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-05-22 21:00:58 -0600

    Updates

---
+++ precache-manifest.a68738b6914731a3355e61740fadae16.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd7009bd9fb3481fa0063cdc0cb3697b",
+    "url": "/swapiBox/index.html"
+  },
+  {
+    "revision": "543990a28a67322e0ff2",
+    "url": "/swapiBox/static/css/main.99938e3e.chunk.css"
+  },
+  {
+    "revision": "117367db765c38d01bb5",
+    "url": "/swapiBox/static/js/2.d8c91e0c.chunk.js"
+  },
+  {
+    "revision": "543990a28a67322e0ff2",
+    "url": "/swapiBox/static/js/main.d27c70ce.chunk.js"
+  },
+  {
+    "revision": "39ff31bcd038e3947029",
+    "url": "/swapiBox/static/js/runtime~main.33bac6c8.js"
+  },
+  {
+    "revision": "37da0be547597a78de6b6a606b7be411",
+    "url": "/swapiBox/static/media/logo.37da0be5.svg"
+  },
+  {
+    "revision": "be6dccbf59c48f6dcccfeba28d74d990",
+    "url": "/swapiBox/static/media/millennium-falcon.be6dccbf.svg"
+  },
+  {
+    "revision": "8a2702bbc3d5e61ae3f6b2a10b26760c",
+    "url": "/swapiBox/static/media/star-wars-people.8a2702bb.svg"
+  },
+  {
+    "revision": "78c8aa6a8a2cd08904bf870f773c5a31",
+    "url": "/swapiBox/static/media/world.78c8aa6a.svg"
+  }
+]);
